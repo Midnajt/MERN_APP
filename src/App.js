b990dc35b1e2856ed0f1ef10dd2c5db1c77d73b7
Marcin K <m.krzysztoszek@cermotor.com.pl>
2024-07-31 13:57:12 +0200

    section 4 finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from "react";
+
+import "./App.css";
+import GoalList from "./components/GoalList/GoalList";
+import NewGoal from "./components/NewGoal/NewGoal";
+
+const DUMMY_GOALS = [
+  { id: "cg1", text: "Finish the Course" },
+  { id: "cg2", text: "Learn all about the Course Main Topic" },
+  { id: "cg3", text: "Help other students in the Course Q&A" },
+];
 
 const App = () => {
-  return <h1>A React App!</h1>;
+  const [courseGoals, setCourseGoals] = useState(DUMMY_GOALS);
+
+  const addNewGoalHandler = (newGoal) => {
+    setCourseGoals((prevCourseGoals) => prevCourseGoals.concat(newGoal));
+  };
+
+  return (
+    <div className="course-goals">
+      <h2>Course Goals</h2>
+      <NewGoal onAddGoal={addNewGoalHandler} />
+      <GoalList goals={courseGoals} />
+    </div>
+  );
 };
 
 export default App;
