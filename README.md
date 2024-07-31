d5ad1b763095707ba7cee64b516b82a9c1fe19f5
Marcin K <m.krzysztoszek@cermotor.com.pl>
2024-07-31 14:04:42 +0200

    starting setup

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # MERN_APP
+
 This repository is dedicated to tracking my progress in mastering the MERN stack. Here, you'll find all my projects, assignments, and notes from the MERN course I'm currently enrolled in.
 
 Technologies Covered:
@@ -7,4 +8,4 @@ Express.js 🚀: Web application framework for Node.js.
 React ⚛️: JavaScript library for building user interfaces.
 Node.js 🟩: JavaScript runtime built on Chrome's V8 engine.
 
-Feel free to explore and follow along as I dive deep into the MERN stack! Any feedback or suggestions are highly appreciated. Let's code! 💻✨
+Feel free to explore and follow along as I dive deep into the MERN stack! Any feedback or suggestions are highly appreciated. 💻✨
--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
-import React, { useState } from "react";
+import React from 'react';
 
-import "./App.css";
-import GoalList from "./components/GoalList/GoalList";
-import NewGoal from "./components/NewGoal/NewGoal";
-
-const DUMMY_GOALS = [
-  { id: "cg1", text: "Finish the Course" },
-  { id: "cg2", text: "Learn all about the Course Main Topic" },
-  { id: "cg3", text: "Help other students in the Course Q&A" },
-];
-
-const App = () => {
-  const [courseGoals, setCourseGoals] = useState(DUMMY_GOALS);
-
-  const addNewGoalHandler = (newGoal) => {
-    setCourseGoals((prevCourseGoals) => prevCourseGoals.concat(newGoal));
-  };
-
-  return (
-    <div className="course-goals">
-      <h2>Course Goals</h2>
-      <NewGoal onAddGoal={addNewGoalHandler} />
-      <GoalList goals={courseGoals} />
-    </div>
-  );
-};
+function App() {
+  return <h1>Let's start!</h1>;
+}
 
 export default App;
