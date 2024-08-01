67e7187da4075f8f24a2f9071034c17bc6c75e51
Marcin K <m.krzysztoszek@cermotor.com.pl>
2024-08-01 14:39:00 +0200

    section 4 finished

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,31 @@
+import Wrapper from "../assets/wrappers/LandingPage";
+import main from "../assets/images/main.svg";
+import { Link } from "react-router-dom";
+import { Logo } from "../components/index";
+
 function Landing() {
-  return <h1>Landing</h1>;
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className='container page'>
+        <div className='info'>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>I m baby +1 thundercats fit pickled chillwave raclette williamsburg migas keytar mukbang neutra hella. Man braid health goth glossier 3 wolf moon next level.</p>
+          <Link to='/register' className='btn register-link'>
+            Register
+          </Link>
+          <Link to='/login' className='btn'>
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </Wrapper>
+  );
 }
+
 export default Landing;
