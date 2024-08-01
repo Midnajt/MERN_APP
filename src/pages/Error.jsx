c9a5af10e17a6d45d4e912d0d2aae12cdbbc6cb3
Marcin K <m.krzysztoszek@cermotor.com.pl>
2024-08-01 14:48:54 +0200

    section finished

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,14 +1,29 @@
 import { Link, useRouteError } from "react-router-dom";
+import Wrapper from "../assets/wrappers/ErrorPage";
+import logo from "../assets/images/not-found.svg";
 
 function Error() {
   const error = useRouteError();
-  console.log(error);
+
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={logo} alt='not found' />
+          <h3>Page not found</h3>
+          <p>We can t to find the page you are looking for</p>
+          <Link to='/dashboard'>back home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
+
   return (
-    <div>
-      <h1>Error 404</h1>
-      <p>{error.data}</p>
-      <Link to='/'>back home</Link>
-    </div>
+    <Wrapper>
+      <div>
+        <h3>Something went wrong</h3>
+      </div>
+    </Wrapper>
   );
 }
 export default Error;
