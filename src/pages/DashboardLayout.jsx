57fed5f59a3f4b7a1d3fdb6f158658517e923a1e
Marcin K <m.krzysztoszek@cermotor.com.pl>
2024-08-02 15:50:42 +0200

    context, icons, navbar with links created

--- src/pages/DashboardLayout.jsx
+++ src/pages/DashboardLayout.jsx
@@ -1,4 +1,48 @@
+import { createContext, useContext, useState } from "react";
+import { Outlet } from "react-router-dom";
+
+import Wrapper from "../assets/wrappers/Dashboard";
+import SmallSideBar from "../components/SmallSidebar";
+import BigSidebar from "../components/BigSidebar";
+import Navbar from "../components/Navbar";
+
+const DashboardContext = createContext();
+
 function DashboardLayout() {
-  return <h1>DashboardLayout</h1>;
+  // temp
+  const user = { name: "john" };
+  const [showSidebar, setShowBar] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const toggleDarkTheme = () => {
+    console.log("toggleDarkTheme");
+  };
+
+  const toggleSidebar = () => {
+    setShowBar(!showSidebar);
+  };
+
+  const logOutUser = () => {
+    console.log("logOutUser");
+  };
+
+  return (
+    <DashboardContext.Provider value={{ user, showSidebar, isDarkTheme, toggleDarkTheme, toggleSidebar, logOutUser }}>
+      <Wrapper>
+        <main className='dashboard'>
+          <SmallSideBar />
+          <BigSidebar />
+          <div>
+            <Navbar />
+            <div className='dashboard-page'>
+              <Outlet />
+            </div>
+          </div>
+        </main>
+      </Wrapper>
+    </DashboardContext.Provider>
+  );
 }
+
+export const useDashboardContext = () => useContext(DashboardContext);
 export default DashboardLayout;
