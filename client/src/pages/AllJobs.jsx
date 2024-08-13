53b8a944be307db98307b660fae6b36821e0e7b2
Marcin K <m.krzysztoszek@cermotor.com.pl>
2024-08-13 13:20:18 +0200

    section finished

--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -21,8 +21,6 @@ const AllJobsContext = createContext();
 const AllJobs = () => {
   const { data } = useLoaderData();
 
-  console.log(data);
-
   return (
     <AllJobsContext.Provider value={{ data }}>
       <SearchContainer />
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,7 +2,6 @@ import { UnauthenticatedError, UnauthorizedError } from "../errors/customErrors.
 import { verifyJWT } from "../utils/tokenUtils.js";
 
 export const authenticateUser = async (req, res, next) => {
-  console.log("Cookies:", req.cookies);
   const { token } = req.cookies;
   if (!token) throw new UnauthenticatedError("authentication invalid");
 
