21ba05877298f7f1530470d3f15078421d5e2ef4
Marcin K <m.krzysztoszek@cermotor.com.pl>
2024-08-19 15:01:47 +0200

    section finished

--- client/src/components/FormRow.jsx
+++ client/src/components/FormRow.jsx
@@ -1,10 +1,10 @@
-const FormRow = ({ type, name, labelText, defaultValue }) => {
+const FormRow = ({ type, name, labelText, defaultValue, onChange }) => {
   return (
     <div className='form-row'>
       <label htmlFor={name} className='form-label'>
         {labelText || name}
       </label>
-      <input type={type} id={name} name={name} className='form-input' defaultValue={defaultValue || ""} required />
+      <input type={type} id={name} name={name} className='form-input' defaultValue={defaultValue || ""} required onChange={onChange} />
     </div>
   );
 };
